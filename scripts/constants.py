6ec24c7ee849c7e8420f287d43f3e16dca8f4d3a
nsprenkle <nathan.sprenkle@gmail.com>
2022-06-22 11:56:17 -0400

    move constants into a new file

---
+++ scripts/constants.py
@@ -0,0 +1,7 @@
+"""Constants used across multiple files"""
+
+VDJ_EXPORT_DIR = "vdj-export"
+PROCESSED_FILES_DIR = "processed-files"
+
+VDJ_DB_FILE = f"{VDJ_EXPORT_DIR}/database.xml"
+JSON_DB_FILE = f"{PROCESSED_FILES_DIR}/database.json"
