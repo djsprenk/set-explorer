f71040d6ce5fa184f56fd4d435cfebf996c3c029
nsprenkle <nathan.sprenkle@gmail.com>
2023-11-18 19:25:49 -0500

    Refactor cue-file to take set name from input args

--- scripts/cue-file.py
+++ scripts/cue-file.py
@@ -1,24 +1,30 @@
 """
-Given a set name, generate a cue file
+Given a set name, generate a cue file.
+
+Args: Set file name
+
+Outputs: {set file name}.cue file with cue points to processed-files dir
 """
 
 from math import floor
 from os.path import basename, splitext
+import sys
 
 from constants import JSON_DB_FILE, PROCESSED_FILES_DIR
 from utils import read_json_file
 
-SET_FILE_NAME = "Sprenk - Dani & Nate.mp3"
-SET_FILE_TITLE = splitext(SET_FILE_NAME)[0]
-
-INPUT_FILE = f"{PROCESSED_FILES_DIR}/zouk-sets.json"
-OUTPUT_FILE = f"{PROCESSED_FILES_DIR}/{SET_FILE_TITLE}.cue"
+DJ_SET_FILE = f"{PROCESSED_FILES_DIR}/zouk-sets.json"
 
 DJ_NAME = "DJ Sprenk"
 
 # Note: keep these lower case to make case-insensitive compare work
 GENRE_FILTER = "zouk set"
 ARTIST_FILTER = "dj sprenk"
+
+
+def set_name_from_title(title):
+    """ Get title from YYMMDD - {title}.mp3 format"""
+    return splitext(title)[0]
 
 
 def cue_filter(elem):
@@ -43,11 +49,19 @@ def seconds_to_minutes_and_seconds(time):
 
 
 if __name__ == "__main__":
+    # Get set from args
+    if len(sys.argv) == 2:
+        set_file = sys.argv[1]
+        set_title = set_name_from_title(set_file)
+    else:
+        print(f"Expected 1 arg: set file name")
+        exit(1)
+
     songs = read_json_file(JSON_DB_FILE)["VirtualDJ_Database"]["Song"]
-    set_metadata = find_set_metadata(songs, SET_FILE_NAME)
+    set_metadata = find_set_metadata(songs, set_file)
 
     if set_metadata is None:
-        print(f"No set found in database for file {SET_FILE_NAME}")
+        print(f"No set found in database for file {set_file}")
         exit(1)
 
     cue_points = filter(cue_filter, set_metadata["Poi"])
@@ -56,8 +70,8 @@ if __name__ == "__main__":
 
     # Write Header
     text_data += f'PERFORMER "{DJ_NAME}"\n'
-    text_data += f'TITLE "{SET_FILE_TITLE}"\n'
-    text_data += f'FILE "{SET_FILE_NAME}"\n'
+    text_data += f'TITLE "{set_title}"\n'
+    text_data += f'FILE "{set_file}"\n'
 
     for cue in cue_points:
         cue_number = cue.get("@Num", "0").zfill(2)
@@ -75,6 +89,9 @@ if __name__ == "__main__":
         text_data += f'    PERFORMER "{performer}"\n'
         text_data += f"    INDEX 01 {timestamp}\n"
 
+    output_file = f"{PROCESSED_FILES_DIR}/{set_title}.cue"
+
     # Write the json data to output json file
-    with open(OUTPUT_FILE, "w") as cue_file:
+    with open(output_file, "w") as cue_file:
         cue_file.write(text_data)
+        print(f"Wrote CUE file to {output_file}")
