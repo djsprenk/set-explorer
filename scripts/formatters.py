854b8750105f4c1f55aa8e43adf9f2fbcd3223a5
nsprenkle <nathan.sprenkle@gmail.com>
2024-01-15 16:13:14 -0500

    refactors formatters

--- scripts/formatters.py
+++ scripts/formatters.py
@@ -1,22 +1,13 @@
 """Different formatters for time, BPM, etc."""
 
 
+from math import floor
+
+
 def bpm_formatter(database):
     for song in database["VirtualDJ_Database"]["Song"]:
         bpm = song.get("Tags", {}).get("@Bpm")
         yield song
-
-
-def time_format(seconds):
-    """
-    Convert seconds to formatted hh:mm:ss string or empty
-    """
-    if not seconds:
-        return ""
-    m, s = divmod(int(float(seconds)), 60)
-    h, m = divmod(m, 60)
-
-    return f"{h:d}:{m:02d}:{s:02d}"
 
 
 def bpm_format(spb):
@@ -28,3 +19,24 @@ def bpm_format(spb):
         return ""
 
     return round(60 / float(spb), 1)
+
+
+def seconds_to_hours_minutes_and_seconds(seconds):
+    """
+    Convert seconds to formatted hh:mm:ss string or empty
+    """
+    if not seconds:
+        return ""
+    m, s = divmod(int(float(seconds)), 60)
+    h, m = divmod(m, 60)
+
+    return f"{h:d}:{m:02d}:{s:02d}"
+
+
+def seconds_to_minutes_and_seconds(time):
+    """Convert cue format ss.mm to MM:ss:mm format"""
+    rounded_seconds = floor(float(time))
+    minutes = floor(rounded_seconds / 60)
+    seconds = rounded_seconds % 60
+
+    return f"{str(minutes).zfill(2)}:{str(seconds).zfill(2)}:00"
