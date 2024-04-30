d498b81eecf5db3d3f58836221cee822817fc3c7
nsprenkle <nathan.sprenkle@gmail.com>
2024-04-30 16:14:35 -0400

    Update constants for new folder structure

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,8 +1,9 @@
 """Constants used across multiple files"""
 
-VDJ_EXPORT_DIR = "vdj-export"
-PROCESSED_FILES_DIR = "processed-files"
-MIXCLOUD_EXPORT_DIR = "mixcloud-export"
+DATA_DIR = "data"
+VDJ_EXPORT_DIR = f"{DATA_DIR}/vdj-database"
+PROCESSED_FILES_DIR = f"{DATA_DIR}/processed-files"
+MIXCLOUD_EXPORT_DIR = f"{DATA_DIR}/mixcloud-export"
 
 VDJ_DB_FILE = f"{VDJ_EXPORT_DIR}/database.xml"
 JSON_DB_FILE = f"{PROCESSED_FILES_DIR}/database.json"
