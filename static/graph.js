56944cad637a94f78bb2fe44b7fc8eee52857a03
nsprenkle <nathan.sprenkle@gmail.com>
2024-05-18 00:05:29 -0400

    Move most styling into CSS stylesheet

--- static/graph.js
+++ static/graph.js
@@ -12,71 +12,46 @@ function mapColor (data, index) {
   return colors[data.Energy || '0']
 }
 
+function mapHeight (data, index) {
+  return `${Math.max(data.Energy, 1) * 4}px`
+}
+
 function getSongMeta (data, index) {
   const artist = data.Artist || 'Unknown'
   const title = data.Title || 'Unknown'
   return `${artist} - ${title}`
 }
 
-function timelineGraph (timelineContainer, data, title) {
-  const timelineWidth = 500
-  const timelineHeight = 20
-  const elementWidth = Math.min(20, timelineWidth / data.length)
+function timelineGraph (container, data, title) {
+  // Add flexbox container for timeline
+  const timelineContainer = container
+    .append('div')
+    .attr('class', 'timeline-container')
 
-  // Add title
+  // Add set title
   timelineContainer
     .append('div')
+    .attr('class', 'title')
     .text(title)
-    .style('display', 'inline-block')
-    .style('padding', '20px')
 
   // Build the timeline group
   const timeline = timelineContainer
-    .append('svg')
-    .attr('width', timelineWidth)
-    .attr('height', timelineHeight)
-    .append('g')
+    .append('div')
+    .attr('class', 'timeline')
 
-  // Map data to rectangles
-  timeline.selectAll('rect')
+  // Map data to divs
+  timeline.selectAll('div')
     .data(data)
-    .join('rect')
+    .join('div')
+    .attr('class', 'song')
 
-  // Block X values are just multiples of width
-    .attr('x', function (d, i) {
-      return i * elementWidth
-    })
-
-  // Entry should display as a square, equal height / width
-    .attr('width', elementWidth)
-    .attr('height', timelineHeight)
-
-  // Since this is a horizontal timeline, Y value is always 0
-    .attr('y', 0)
+  // Block order is the index
+    .style('order', function (d, i) { return i })
 
   // Color the rectangles with their energies
-    .style('fill', mapColor)
-    .style('stroke', 'white')
-    .style('stroke-width', '1px')
-    .text(getSongMeta)
-
-  // Hover Effects
-  // Source: https://medium.com/@kj_schmidt/show-data-on-mouse-over-with-d3-js-3bf598ff8fc2
-    .on('mouseover', function (d, i) {
-      d3.select(this).transition()
-        .duration('50')
-        .attr('opacity', '.75')
-        .style('stroke-width', '4px')
-    })
-    .on('mouseout', function (d, i) {
-      d3.select(this).transition()
-        .duration('50')
-        .attr('opacity', '1')
-        .style('stroke-width', '1px')
-    })
-
-  // Add break to give space between graphs
-  timelineContainer.append('br')
+    .style('background-color', mapColor)
+    .style('height', mapHeight)
+    .attr('title', getSongMeta)
 }
 
 const container = d3.select('#my_dataviz')
