bcd07f3a9f86c42bb12352c85930a9cf7933f255
nsprenkle <nathan.sprenkle@gmail.com>
2024-05-23 19:53:33 -0400

    Update pandas_ingest to separate live and produced sets

--- scripts/pandas_ingest.py
+++ scripts/pandas_ingest.py
@@ -11,7 +11,8 @@ import pandas as pd
 from constants import PROCESSED_FILES_DIR, SONG_LISTS_DIR, STATIC_DIR
 from utils import read_json_file
 
-sets_data_file = read_json_file(Path(PROCESSED_FILES_DIR, "sets-data.json"))
+live_sets = read_json_file(Path(PROCESSED_FILES_DIR, "live-sets-data.json"))
+produced_sets = read_json_file(Path(PROCESSED_FILES_DIR, "produced-sets-data.json"))
 OUTPUT_FILE_PATH = Path(STATIC_DIR, "song-data.js")
 
 sets_data = []
@@ -19,7 +20,12 @@ sets_data = []
 mixcloud_slugs_raw = read_json_file(Path(PROCESSED_FILES_DIR, "mixcloud-slugs.json"))
 mixcloud_slugs = pd.json_normalize(mixcloud_slugs_raw["data"])
 
-for set_data in sets_data_file:
+
+def extract_song_data_for_playlist(set_data, additional_meta):
+    """Get song data for a playlist file.
+
+    Returns dict of cleaned and formatted data.
+    """
     playlist_file = Path(SONG_LISTS_DIR, set_data["playlist"])
     raw_song_data = read_json_file(playlist_file.with_suffix(".json"))
 
@@ -33,6 +39,13 @@ for set_data in sets_data_file:
         }
     )
 
+    # A few, rare playlists don't have any songs with energy associated, zero them out
+    if song_data.get("Energy") is None:
+        song_data["Energy"] = 0
+
+    # Fill NaN values
+    song_data["Energy"] = song_data["Energy"].fillna(0)
+
     # Find match in mixcloud_slugs
     matches = mixcloud_slugs.index[
         mixcloud_slugs["slug"] == set_data.get("slug")
@@ -40,22 +53,34 @@ for set_data in sets_data_file:
 
     # For now, skip anything not published to mixcloud
     if not matches:
-        continue
+        return
     mixcloud_data = mixcloud_slugs.iloc[matches[0]] if matches else {}
 
     song_data["index"] = [i for i in range(len(song_data))]
     song_data["set"] = playlist_file.name
     song_energy = song_data[["index", "Title", "Artist", "Energy"]]
 
-    sets_data.append(
-        {
-            "title": mixcloud_data.get("name") or playlist_file.name,
-            "url": mixcloud_data.get("url"),
-            "uploadTimestamp": mixcloud_data.get("created_time"),
-            "img": mixcloud_data.get("pictures.large"),
-            "data": song_energy.to_dict(orient="records"),
-        }
-    )
+    return {
+        "title": mixcloud_data.get("name") or playlist_file.name,
+        "url": mixcloud_data.get("url"),
+        "uploadTimestamp": mixcloud_data.get("created_time"),
+        "img": mixcloud_data.get("pictures.large"),
+        "data": song_energy.to_dict(orient="records"),
+        **additional_meta,
+    }
+
+
+for set_data in live_sets:
+    extracted_data = extract_song_data_for_playlist(set_data, {"type": "live"})
+    if not extracted_data:
+        continue
+    sets_data.append(extracted_data)
+
+for set_data in produced_sets:
+    extracted_data = extract_song_data_for_playlist(set_data, {"type": "produced"})
+    if not extracted_data:
+        continue
+    sets_data.append(extracted_data)
 
 with open(OUTPUT_FILE_PATH, "w", encoding="utf-8") as output_file:
     output_file.write("const songData = ")
